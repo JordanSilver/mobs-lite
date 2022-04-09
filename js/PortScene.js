d8895bc0d469bcdc47b4c1e43da5c2a96d1988a0
Daniel Silver <dabzbm@gmail.com>
2022-04-09 02:17:23 -0400

    portfolio websites

--- js/PortScene.js
+++ js/PortScene.js
@@ -10,7 +10,7 @@ const portWorldBg = new Sprite({
 });
 
 // port map speed
-const portSpeed = 6;
+const portSpeed = 7;
 
 // DRAW EXIT PORT BARRIER
 const exitPortMap = [];
@@ -24,6 +24,29 @@ exitPortMap.forEach((row, i) => {
   row.forEach((symbol, j) => {
     if (symbol === 393) {
       exitPortZones.push(
+        new Boundary({
+          position: {
+            x: j * Boundary.width + offset.x,
+            y: i * Boundary.height + offset.y,
+          },
+        })
+      );
+    }
+  });
+});
+
+// DRAW Enter WEB BARRIER
+const enterWebMap = [];
+for (let i = 0; i < enterWebData.length; i += 70) {
+  enterWebMap.push(enterWebData.slice(i, 70 + i));
+}
+
+const enterWebZones = [];
+
+enterWebMap.forEach((row, i) => {
+  row.forEach((symbol, j) => {
+    if (symbol === 393) {
+      enterWebZones.push(
         new Boundary({
           position: {
             x: j * Boundary.width + offset.x,
@@ -60,7 +83,12 @@ portCollisionMap.forEach((row, i) => {
 });
 
 //
-const portMovers = [portWorldBg, ...boundari, ...exitPortZones];
+const portMovers = [
+  portWorldBg,
+  ...boundari,
+  ...exitPortZones,
+  ...enterWebZones,
+];
 
 function animatePort() {
   const portAniID = window.requestAnimationFrame(animatePort);
@@ -78,6 +106,10 @@ function animatePort() {
   // DRAW exit ZONES
   exitPortZones.forEach((exitZone) => {
     exitZone.draw();
+  });
+  // DRAW ENTER WEB ZONES
+  enterWebZones.forEach((webZone) => {
+    webZone.draw();
   });
 
   let mova = true;
@@ -143,6 +175,72 @@ function animatePort() {
       }
     }
   }
+  let url;
+
+  const playerLocation = () => {
+    if (portWorldBg.position.x >= -300 && portWorldBg.position.y >= -650) {
+      url = 'https://tagpainting.com';
+    } else if (
+      portWorldBg.position.x >= -300 &&
+      portWorldBg.position.y <= -900
+    ) {
+      url = 'https://nxt-crypto.netlify.app/';
+    }
+  };
+
+  playerLocation();
+  // // WEB ZONE DETECTION
+  if (keys.w.pressed || keys.a.pressed || keys.s.pressed || keys.d.pressed) {
+    for (let i = 0; i < enterWebZones.length; i++) {
+      const webZone = enterWebZones[i];
+
+      const overlappingArea =
+        (Math.min(
+          player.position.x + player.width,
+          webZone.position.x + webZone.width
+        ) -
+          Math.max(player.position.x, webZone.position.x)) *
+        (Math.min(
+          player.position.y + player.height,
+          webZone.position.y + webZone.height
+        ) -
+          Math.max(player.position.y, webZone.position.y));
+
+      if (
+        rectCollision({
+          rect1: player,
+          rect2: webZone,
+        }) &&
+        overlappingArea > (player.width * player.height) / 4
+      ) {
+        // DEACTIVATE CURRENT ANIMATION LOOP
+        if (keys.w.pressed) {
+          gsap.to('#transition', {
+            opacity: 1,
+            repeat: 1,
+            yoyo: true,
+            duration: 0.2,
+            onComplete() {
+              gsap.to('#transition', {
+                opacity: 1,
+                duration: 0.4,
+                onComplete() {
+                  // ACTIVE NEW ANIMATION LOOP
+                  audio.House.stop();
+                  window.open(url, '_blank');
+                  gsap.to('#transition', {
+                    opacity: 0,
+                    duration: 0.4,
+                  });
+                },
+              });
+            },
+          });
+        }
+        break;
+      }
+    }
+  }
 
   // moving port with collision detection
 
@@ -158,7 +256,7 @@ function animatePort() {
             ...boundary,
             position: {
               x: boundary.position.x,
-              y: boundary.position.y + portSpeed,
+              y: boundary.position.y - player.height / 2 + portSpeed,
             },
           },
         })
@@ -182,7 +280,7 @@ function animatePort() {
             ...boundary,
             position: {
               x: boundary.position.x,
-              y: boundary.position.y - portSpeed,
+              y: boundary.position.y - player.height / 2 - portSpeed,
             },
           },
         })
